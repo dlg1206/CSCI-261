877c7985a4c887743d1b200b8922a20435855711
Derek Garcia <dlg1206@g.rit.edu>
2022-01-21 16:54:25 -0500

    Removed print statements

--- Homeworks/HW1/Stable.java
+++ Homeworks/HW1/Stable.java
@@ -73,12 +73,10 @@ public class Stable {
 
                     // Cycle through man's preferences
                     for (String woman : people.get(man)) {
-                        System.out.println(man + " is proposing to " + woman);
 
                         // If woman free, engage
                         if (!matches.containsValue(woman)) {
-                            System.out.println(man + " and " + woman + " are engaged!");
-                            matches.put(man, woman);
+                            matches.put(man, woman);    // engage
                             break;
                         }
                     }
@@ -89,38 +87,19 @@ public class Stable {
 
                         // If woman's preference not engaged, leave current man for preference
                         if (!matches.containsKey(wPref)) {
-                            System.out.println("\t" +woman + " left " + man + " for " + wPref + "...");
                             matches.put(wPref, woman);  // new engagement
                             matches.remove(man);    // old man set as "free" (ie unmatched)
-                            System.out.println(wPref + " and " + woman + " are engaged!");
                             break;
                         }
 
                         // If woman's preference matches current man, stay engaged
                         if (wPref.equals(man)) {
-                            System.out.println("\t" + woman + " is a happy with " + man + " :)");
                             break;  // no need to cont. checking
                         }
                     }
-                } else {
-
-                    if(man.contains("m")){
-                        System.out.println(man + " is already engaged");
-                    } else {
-                        System.out.println("\t" + man + " is a woman 0.o");
-                    }
-
                 }
             }
         }
-
-        // replace
-        System.out.println("Results");
-        for (String man : matches.keySet()) {
-
-            System.out.println("(" + man + ", " + matches.get(man) + ")");
-        }
-
     }
 
 
