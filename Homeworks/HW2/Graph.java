c780107fa23f931f5fcc8f533a218bc7e69cba12
Derek Garcia <dlg1206@g.rit.edu>
2022-02-15 15:42:32 -0500

    Parse input file correctly

--- Homeworks/HW2/Graph.java
+++ Homeworks/HW2/Graph.java
@@ -1,6 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * @author Derek Garcia
  **/
 
 public class Graph {
+
+
+    /**
+     * Parses input file to create a hashmap graph of given values
+     *
+     * @param fileName name of input file
+     * @return graph
+     * @throws IOException filename is bad
+     */
+    private static HashMap<Integer, Node> parseFile(String fileName) throws IOException {
+        // Create new Buffered Reader
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        br.readLine();  // trash first line
+
+        HashMap<Integer, Node> graph = new HashMap<>();
+
+        String line = br.readLine();    // get first node info
+
+        int nodeID = 1; // nodes start at 1
+
+        // Read entire file
+        while (line != null){
+
+            String[] test = line.split(" ");    // gets adj nodes
+
+            // init node if needed
+            if(!graph.containsKey(nodeID)){
+                graph.put(nodeID, new Node(nodeID));
+            }
+
+            Node curNode = graph.get(nodeID);
+
+            boolean skippedFirst = false;
+
+            // Add all adj nodes
+            for(String adj : test){
+                // skip over "Node[n]:"
+                if(!skippedFirst){
+                    skippedFirst = true;
+                    continue;
+                }
+
+                int adjID = Integer.parseInt(adj);
+
+                // Init adj node if needed
+                if(!graph.containsKey(adjID)){
+                    graph.put(adjID, new Node(adjID));
+                }
+
+                curNode.addEdge(graph.get(adjID));
+            }
+
+            nodeID++;   // move to next node
+            line = br.readLine();
+        }
+
+        br.close();
+        return graph;
+    }
+
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, Node> graph = parseFile(args[0]);
+
+    }
+
 }
