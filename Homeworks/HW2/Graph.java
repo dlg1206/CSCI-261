838e7699b6a88dda37415f509ae2d87e2986935f
Derek Garcia <dlg1206@g.rit.edu>
2022-02-16 13:48:18 -0500

    added docstrings

--- Homeworks/HW2/Graph.java
+++ Homeworks/HW2/Graph.java
@@ -72,36 +72,49 @@ public class Graph {
         return graph;
     }
 
+
+    /**
+     * Does a breadth first search of a given graph at a start node
+     * Will recursively go through graph if it detects not all nodes were reached
+     *
+     * @param componentNum Component number / recursive depth
+     * @param startNode Node to start at
+     * @param graph graph to search through
+     * @return updated graph with nodes set to a layer on a BFS tree
+     */
     private static HashMap<Integer, Node> doBFS(int componentNum, int startNode, HashMap<Integer, Node> graph){
 
         // init vars
         LinkedList<Node> visitQueue = new LinkedList<>();
-
         boolean bipartite = true;
 
+        // Get initial node
         Node curNode = graph.get(startNode);
         curNode.layer = 0;
 
         System.out.println("connected component: " + componentNum);
+
+        // Visit all nodes connected to start node
         for(;;){
+            System.out.print(curNode.id + "(" + curNode.layer +") ");   // ex. 1(0)
 
-            System.out.print(curNode.id + "(" + curNode.layer +") ");
-
+            // Visit all nodes adjacent to current node
             for(Node adj : curNode.adj){
 
-                // If adj hasn't been visited
+                // Add to visit queue if not visited
                 if(adj.layer < 0){
-                    adj.layer = curNode.layer + 1;
+                    adj.layer = curNode.layer + 1;  // adj will always be on the next layer
                     visitQueue.add(adj);
                 }
 
+                // Bipartite detection
                 if(adj.layer == curNode.layer){
                     bipartite = false;
                 }
 
             }
 
-
+            // Break when nowhere to visit; else get next node
             if(visitQueue.isEmpty()){
                 break;
             } else {
@@ -110,31 +123,33 @@ public class Graph {
 
         }
 
-        System.out.println();
+        System.out.println();   // newline
 
+        // Print bipartite status
         if(bipartite){
             System.out.println(BIPARTITE);
         } else {
             System.out.println(NOT_BIPARTITE);
         }
 
-
         // check for skipped nodes
         for(Node node : graph.values()){
-
-            // -1 + -1 = -2
+            // Do another BFS if skipped node found
             if(node.layer < 0){
                 doBFS(++componentNum, node.id, graph);
             }
-
         }
 
-
-
-
+        // Return updated graph
         return graph;
     }
 
+
+    /**
+     * Prints the nodes and its neighbors in a given graph
+     *
+     * @param graph graph to print
+     */
     private static void printGraph(HashMap<Integer, Node> graph){
 
         System.out.println(graph.size());
@@ -142,12 +157,17 @@ public class Graph {
             System.out.println(node);
         }
 
-        System.out.println();
+        System.out.println();   // newline
 
     }
 
 
-
+    /**
+     * Takes in adjacency file and creates a graph
+     *
+     * @param args file name of adjacent
+     * @throws IOException file name is bad
+     */
     public static void main(String[] args) throws IOException {
         HashMap<Integer, Node> graph = parseFile(args[0]);
         printGraph(graph);
