ad3879c318a973aa06587c86c707aaff9fa90f38
Derek Garcia <dlg1206@g.rit.edu>
2022-03-20 01:19:18 -0400

    p4: commented brute force

--- Homeworks/hw4/p4/BruteForce.java
+++ Homeworks/hw4/p4/BruteForce.java
@@ -1,25 +1,29 @@
 /**
- * Brute force method of finding a closest pair
+ * Brute force method of finding the closest pair of points
+ *
  * @author Derek Garcia
  **/
-
 public class BruteForce extends ClosestPairAlg{
 
+
+    /**
+     * Finds the closest pair of points in a given array using a brute
+     * force method
+     * @param P array of points to search
+     * @return Triple containing a pair of points and their distance
+     */
     public Triple closestPair(Point[] P) {
 
-
-        /*
-        1 - get init point
-        2 for each point, compare w/ current
-        3 if closer than current, update
-         */
-        // init first dist
+        // Init closest pair with first two points
         Triple closestPair = new Triple(P[0], P[1], P[0].distance(P[1]));
 
+        // Start at every point in the array
         for(Point currPoint : P){
 
+            // compare current with all the other nodes
             for(Point otherPoint : P){
 
+                // if not the same point and distance is less than the current, update closest
                 if(currPoint != otherPoint && currPoint.distance(otherPoint) < closestPair.dist){
                     closestPair.p1 = currPoint;
                     closestPair.p2 = otherPoint;
