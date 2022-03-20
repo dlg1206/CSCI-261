229b6e09daa538f95924dfbd9c575381b537567c
Derek Garcia <dlg1206@g.rit.edu>
2022-03-20 01:12:53 -0400

    p4: init brute force complete

--- Homeworks/hw4/p4/BruteForce.java
+++ Homeworks/hw4/p4/BruteForce.java
@@ -4,8 +4,30 @@
 
 public class BruteForce extends ClosestPairAlg{
 
-
     public Triple closestPair(Point[] P) {
-        return null;
+
+
+        /*
+        1 - get init point
+        2 for each point, compare w/ current
+        3 if closer than current, update
+         */
+        // init first dist
+        Triple closestPair = new Triple(P[0], P[1], P[0].distance(P[1]));
+
+        for(Point currPoint : P){
+
+            for(Point otherPoint : P){
+
+                if(currPoint != otherPoint && currPoint.distance(otherPoint) < closestPair.dist){
+                    closestPair.p1 = currPoint;
+                    closestPair.p2 = otherPoint;
+                    closestPair.dist = currPoint.distance(otherPoint);
+                }
+            }
+
+        }
+
+        return closestPair;
     }
 }
