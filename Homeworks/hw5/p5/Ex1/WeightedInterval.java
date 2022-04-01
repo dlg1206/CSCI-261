a3917535f75940ed543a0283bf662c6b05d78903
Derek Garcia <dlg1206@g.rit.edu>
2022-04-01 18:25:16 -0400

    p5: added given files

---
+++ Homeworks/hw5/p5/Ex1/WeightedInterval.java
@@ -0,0 +1,107 @@
+import java.util.*;
+import java.io.*;
+
+public class WeightedInterval {
+
+    public static int [] M;
+    
+    public static void main(String [] args) throws FileNotFoundException {
+        // read input file
+	Scanner sc = new Scanner(new File(args[0]));
+	int n = Integer.parseInt(sc.nextLine());
+	// Jobs are indexed 1 .. n
+	Job [] jobs = new Job[n+1];
+	// Job 0 is a fake job
+	jobs[0] = null;
+	for (int i = 1; i <= n; i++) {
+	    String line = sc.nextLine();
+	    String [] parts = line.split(" ");
+	    int job = Integer.parseInt(parts[0]);
+	    int start = Integer.parseInt(parts[1]);
+	    int finish = Integer.parseInt(parts[2]);
+	    int weight = Integer.parseInt(parts[3]);
+	    jobs[i] = new Job(job, start, finish, weight);
+	}
+
+	// sort the jobs by finish time
+	Arrays.sort(jobs,1,jobs.length);
+
+	// For each job j, get the index of the first job to the left
+	// of Job j that is compatible with job j
+	int [] p = prior(jobs);
+
+	// Find max sum of weights of compatible jobs recursively.
+	System.out.println("Find max weighted interval recursive ");
+	long start = System.currentTimeMillis();
+	int opt = optR(jobs, p, jobs.length-1);
+	long stop = System.currentTimeMillis();
+	System.out.println("Max weight sum = " + opt);
+	System.out.println("Time = " + (stop-start));
+
+	// Find max sum of weights of compatible jobs using memoization	
+	System.out.println("\nFind max weighted interval iteratively/memoized");
+	start =  System.currentTimeMillis();
+	opt = optMem(jobs, p);
+	stop =   System.currentTimeMillis();
+	System.out.println("Max weight sum = " + opt);
+	System.out.println("Time = " + (stop-start));
+
+	// Find max sum of weights of compatible jobs using memoization		
+	System.out.println("\nSolution: " );
+	showSolution(jobs, p);
+
+    }
+
+
+    /**
+     *
+     * @ param jobs a list of jobs sorted by finish time
+     * @ return int [] p, p[j] is the first job to the left of jobs[j]
+     *                    that is compatible with jobs[j]
+     */
+    public static int [] prior(Job[] jobs) {
+
+        // FINISH ME
+	return null;
+    }
+
+    
+    /**
+     *
+     * param jobs - sorted by ascending finish time
+     * param p - array of indices into jobs, jobs[p[j]] is first job
+     *           to left of Job j compatible with j
+     *       j - the index of job currently under consideration to
+     *           be part of the optimal solution
+     * return max sum of weights of compatible jobs
+     */
+    public static int optR(Job[] jobs, int[] p, int j) {
+	// FINISH ME
+	return 0;
+    }
+
+    /**
+     *
+     * param jobs - sorted by ascending finish time
+     * param p - array of indices into jobs, jobs[p[j]] is first job
+     *           to left of Job j compatible with j
+     * return max sum of weights of compatible jobs
+     */    
+    public static int optMem(Job[] jobs, int[] p) {
+	// FINISH ME
+	return 0;
+    }
+
+
+    // go through array M to find and list of jobs that are part of the
+    // maximum value solution
+    public static void showSolution(Job [] jobs, int [] p) {
+	showSolution(jobs, p, jobs.length-1);
+    }
+
+    public static void showSolution(Job[] jobs, int [] p, int j) {
+	// FINISH ME
+	return;
+    }    
+
+}
---
+++ Homeworks/hw5/p5/Ex2/SubsetSum.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class SubsetSum{
+    public static int [][] M;
+
+    public static void main (String [] args) throws FileNotFoundException {
+        // read input file
+	Scanner sc = new Scanner(new File(args[0]));
+        int W = Integer.parseInt(args[1]);
+	int n = Integer.parseInt(sc.nextLine());
+	int [] itemWts = new int[n+1];
+	itemWts[0] = 0;
+	for (int i = 1; i <= n; i++)
+	    itemWts[i] = Integer.parseInt(sc.nextLine());
+
+	long start = System.currentTimeMillis();
+	int maxWeight = subsetSumMem(itemWts, W );
+	long stop = System.currentTimeMillis();
+	System.out.println("Memoized: Max Weight for " + n + " items = " +
+			   maxWeight);
+	System.out.println("Time = " + (stop-start));
+        System.out.println("\nSolution");
+        showSolution(itemWts, W, itemWts.length-1);
+	
+	start = System.currentTimeMillis();
+	maxWeight = subsetSumR(itemWts, W, n);
+	stop = System.currentTimeMillis();
+	System.out.println("\nRecursive: Max Weight for " + n + " items = " +
+			   maxWeight);
+	System.out.println("Time = " + (stop-start));
+    }
+
+    /**
+     *
+     * @ param int [] itemWts - an array of the weights of items, itemWts[i]
+     *                          is the weight of item i
+     *         int W - the capacity allowed
+     * @ return int - maximized sum of weights from itemWts <= W
+     *                    that is compatible with jobs[j]
+     */
+    public static int subsetSumMem(int [] itemWts, int W) {
+        M = new int [itemWts.length][W+1];
+	// FINISH ME
+	return 0;
+
+    }
+
+    /**
+     *
+     * @ param int [] itemWts - an array of the weights of items, itemWts[i]
+     *                          is the weight of item i
+     *         int w - the current capacity allowed
+     *         int i - index of item under consideration
+     * @ return int - maximized sum of weights from itemWts <= W
+     *                    that is compatible with jobs[j]
+     */
+    public static int subsetSumR(int [] itemWts, int w, int i) {
+
+	// FINISH ME
+	return 0;
+    }
+
+    /**
+     *
+     * @ param int [] itemWts - an array of the weights of items, itemWts[i]
+     *                          is the weight of item i
+     *         int w - the current capacity allowed
+     *         int i - index of item under consideration
+     * @ return int - maximized sum of weights from itemWts <= W
+     *                    that is compatible with jobs[j]
+     */
+    public static void showSolution(int [] itemWts, int w, int i) {
+	// FINISH ME
+    }
+    
+}
